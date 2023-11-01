e60eec7b273b69c857f1fd700a7e3780cfa9397f
soony1995 <59558831+soony1995@users.noreply.github.com>
2023-11-01 15:49:01 +0900

    코딩 테스트

---
+++ coding_test/python_중복된_문자_제거.py
@@ -0,0 +1,32 @@
+문제 = `문자열 my_string이 매개변수로 주어집니다. my_string에서 중복된 문자를 제거하고 하나의 문자만 남긴 문자열을 return하도록 solution 함수를 완성해주세요.`
+
+>> string 의 각 인덱스별로 dict 타입으로 변환하고 싶을 때 사용하는 키워드 
+
+1. dict.fromkeys(my_string) 
+
+입력값 〉
+  	"people"
+기댓값 〉
+  	"peol"
+실행 결과 〉
+	실행한 결괏값 ""이 기댓값 "peol"과 다릅니다.
+출력 〉
+    {'p': None, 'e': None, 'o': None, 'l': None}
+
+2. dict 타입을 string 타입으로 변환. ''.join()메서드 이용.
+
+
+
+def solution(my_string): 
+    answer = ""
+    for i in my_string:
+        if i not in answer:
+            answer += i                
+    return answer
+
+    
+def solution(my_string):
+    return ''.join(dict.fromkeys(my_string)) 
+
+
+
